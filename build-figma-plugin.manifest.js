8eabc34ae2255ed3ec664de42bbc626c32b8df3e
Tobias <this@tobias-schwerdtfeger.dev>
2023-07-07 12:28:23 +0200

    Restrict network access

---
+++ build-figma-plugin.manifest.js
@@ -0,0 +1,12 @@
+// workaround for https://github.com/yuanqing/create-figma-plugin/issues/164
+
+module.exports = function (manifest) {
+    return {
+        ...manifest,
+        networkAccess: {
+            allowedDomains: [
+                "none"
+            ]
+        }
+    }
+}
